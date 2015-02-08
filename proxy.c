a3c59258ac2c791e859802ec642884ddd8e829e2
Jesse Hu <huyouare@gmail.com>
2015-02-07 23:33:39 -0500

    Fix pthread initialization, re-implement forwarder function with threading

--- proxy.c
+++ proxy.c
@@ -52,7 +52,6 @@ int main(int argc, char *argv[])
   char *haddrp;
   sigset_t sig_pipe; 
   pthread_t tid;
-  int *args;
   
   if (argc < 2) {
     printf("Usage: ./%s port [debug] [webServerPort]\n", argv[0]);
@@ -113,14 +112,16 @@ int main(int argc, char *argv[])
     char *message = "Thread 1";
     /* create a new thread (or two) to process the new connection */
 
+    int *args = malloc(sizeof(*args));
     args[0] = connfd;
     args[1] = serverPort;
 
-    if (pthread_create(&thread, NULL, webTalk(args), (void*) message) < 0) {
+    if (pthread_create(&thread, NULL, &webTalk, args) < 0) {
       // printf("Client thread error\n");
     }
+    // pthread_detach(&thread);
+    // Close(connfd);
 
-    printf("Done Thread\n");
     printf("Done Thread\n");
   }
 
@@ -203,7 +204,9 @@ void *webTalk(void* args)
   char target_address[MAXLINE];
   char path[MAXLINE];
   int *port = &serverPort;
+  char request_copy[MAXLINE];
 
+  strcpy(request_copy, request);
   uri = strchr(request, 'h');
 
   strtok_r(uri, " ", &saveptr);
@@ -219,70 +222,12 @@ void *webTalk(void* args)
     // GET: open connection to webserver (try several times, if necessary)
 
     serverfd = Open_clientfd(target_address, *port);
-
     printf("Connected!\n");
 
-    // Write(sockfd, request, MAXLINE);
-    // int n;
-    // while (((n = Rio_readlineb(&client, buf1, MAXLINE)) > 0) && (buf1[0] != '\r')) {
-    //   // if (buf1[0] != 'C') {
-    //     printf("%s", buf1);
-    //     Write(sockfd, buf1, MAXLINE);
-    //   // }
-    // }
-    // printf("Start server read\n");
-    // int len = recv(sockfd, buf2, MAXLINE, 0);
-    // buf2[len] = '\0';
-    // printf("%s\n", buf2);
-
-    // while (((n = Rio_readn(sockfd, buf1, MAXLINE)) > 0) && (buf1[0] != '\r')) {
-    //   printf("Server reading...\n");
-    //   printf("%s\n", buf1);
-    // }
-
-    // int sockfd = Socket(AF_INET, SOCK_STREAM/* use tcp */, 0);
-    // struct addrinfo hints;
-    // struct addrinfo *res;
-    // int status;
-
-    // memset(&hints, 0, sizeof hints);
-    // hints.ai_family = AF_UNSPEC;
-    // hints.ai_socktype = SOCK_STREAM;
-    // hints.ai_flags = AI_PASSIVE;
-
-    // char buffer[10];
-    // sprintf(buffer, "%d", *port);
-
-    // if ((status = getaddrinfo(target_address, buffer, &hints, &res)) != 0) {
-    //     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-    //     exit(1);
-    // }
-
-    // struct addrinfo *r;
-    // r = res;
-    // for(r = res; r != NULL; r = r->ai_next) {
-    //   void *addr;
-    //   char *ipver = "IPV4";
-    //   struct sockaddr_in *ip = (struct sockaddr_in *)r->ai_addr;
-    //   addr = &(ip->sin_addr);
-    //   char ipstr[INET6_ADDRSTRLEN];
-      
-    //   inet_ntop(r->ai_family, addr, ipstr, sizeof ipstr);
-    //   printf("%s: %s\n", ipver, ipstr);
-
-      // int s = socket(r->ai_family, r->ai_socktype, r->ai_protocol);
-
-      // if (Connect(s, ip, sizeof(ip)) < 0) {
-      //   printf("Connection fail.\n");
-      // } else {
-
     /* GET: Transfer first header to webserver */
-
-    // TODO: Switch to HTTP/1.0, Connection closed
-
     send(serverfd, request, MAXLINE, 0);
-    send(serverfd, "\r\n", MAXLINE, 0);
-    printf("%s", request);
+    send(serverfd, "\n", MAXLINE, 0);
+    printf("%s HTTP/1.1\n", request);
 
     // char host[MAXLINE];
     // Rio_readlineb(&client, host, MAXLINE);
@@ -294,25 +239,34 @@ void *webTalk(void* args)
     // GET: Transfer remainder of the request
     int n;
     while (((n = Rio_readlineb(&client, buf1, MAXLINE)) > 0) && (buf1[0] != '\r')) {
+      // TODO: Connection closed
       if (buf1[0] != 'C') {
         printf("%s", buf1);
         send(serverfd, buf1, MAXLINE, 0);
-        send(serverfd, "\r\n", MAXLINE, 0);
+        send(serverfd, "\n", MAXLINE, 0);
       }
     }
     send(serverfd, "\r\n\r\n", MAXLINE, 0);
 
     // GET: now receive the response
     printf("Begin GET\n");
-    int argsF[2];
+    int *argsF = malloc(2 * sizeof(argsF));
     argsF[0] = clientfd;
     argsF[1] = serverfd;
     printf("Call forwarder\n");
-    forwarder(argsF);
+
+    pthread_t thread;
+    char *message = "Thread 1";
+
+    if (pthread_create(&thread, NULL, &forwarder, argsF) < 0) {
+      // printf("Client thread error\n");
+    }
 
   } else {
     // CONNECT: call a different function, securetalk, for HTTPS
-
+    printf("Caling secureTalk\n");
+    char inHost[MAXLINE], version[MAXLINE];
+    secureTalk(clientfd, client, &inHost, &version, *port);
   }
   return (void *)args;
 }
@@ -334,11 +288,11 @@ void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int ser
     serverPort = 443;
   
   /* Open connecton to webserver */
-
+  serverfd = Open_clientfd(target_address, *port);
+  printf("Connected in secureTalk\n");
 
   /* clientfd is browser */
   /* serverfd is server */
-  
   
   /* let the client know we've connected to the server */
 
@@ -354,53 +308,72 @@ void *forwarder(void* args)
 {
   int numBytes, lineNum, serverfd, clientfd;
   int byteCount = 0;
-  char buf[MAXLINE];
+  char buf[MAXLINE], buf1[MAXLINE];
   clientfd = ((int*)args)[0];
   serverfd = ((int*)args)[1];
   // free(args);
 
-  printf("Start server read\n");
+  printf("\nStart server read\n");
 
-  
-
-
-
-  while(1) {
-    
-    /* serverfd is for talking to the web server */
-    int tmpres;
-    memset(buf, 0, sizeof(buf));
-    int htmlstart = 0;
-    char * htmlcontent;
-    htmlstart = 0;
-    while((tmpres = recv(serverfd, buf, MAXLINE, 0)) > 0){
-      if(htmlstart == 0)
-      {
-        /* Under certain conditions this will not work.
-        * If the \r\n\r\n part is splitted into two messages
-        * it will fail to detect the beginning of HTML content
-        */
-        htmlcontent = strstr(buf, "\r\n\r\n");
-        if(htmlcontent != NULL){
-          htmlstart = 1;
-          htmlcontent += 4;
-        }
-      }else{
-        htmlcontent = buf;
-      }
-      if(htmlstart){
-        Rio_writen(clientfd, htmlcontent, MAXLINE);
-      }
-    
-      memset(buf, 0, tmpres);
-    }
-    // if(tmpres < 0)
-    // {
-    //   perror("Error receiving data");
-    // }
-    /* clientfd is for talking to the browser */
-    
+  int i = 0;
+  while(Rio_readp(serverfd, buf, MAXLINE) > 0) {
+      Rio_writen(clientfd, buf, strlen(buf));
+      // printf("%s\n", buf);
   }
+  Close(serverfd);
+  Close(clientfd);
+  // while(Rio_readp(clientfd, buf, MAXLINE) >= 0)
+  //     Rio_writep(serverfd, buf, MAXLINE);
+
+    // i++;
+    // /* serverfd is for talking to the web server */
+    // int tmpres;
+    // memset(buf, 0, sizeof(buf));
+    // int htmlstart = 0;
+    // char * htmlcontent;
+    // htmlstart = 0;
+    // while((tmpres = recv(serverfd, buf, MAXLINE, 0)) > 0){
+    //   if(htmlstart == 0)
+    //   {
+    //     /* Under certain conditions this will not work.
+    //     * If the \r\n\r\n part is splitted into two messages
+    //     * it will fail to detect the beginning of HTML content
+    //     */
+    //     htmlcontent = strstr(buf, "\r\n\r\n");
+    //     if(htmlcontent != NULL){
+    //       htmlstart = 1;
+    //       htmlcontent += 4;
+    //     }
+    //   }else{
+    //     htmlcontent = buf;
+    //   }
+    //   if(htmlstart){
+    //     Rio_writen(clientfd, htmlcontent, MAXLINE);
+    //     printf("%s\n", htmlcontent);
+    //   }
+    
+    //   memset(buf, 0, tmpres);
+    // }
+    // Rio_writen(clientfd, "\r\n", MAXLINE);
+    // printf("Finish server\n");
+    // // if(tmpres < 0)
+    // // {
+    // //   perror("Error receiving data");
+    // // }
+    // /* clientfd is for talking to the browser */
+    // // printf("Finished reading server\n\n");
+    // // printf("Begin sending client request\n");
+    // // GET: Transfer remainder of the request
+    // int n;
+    // while (((n = Rio_readn(clientfd, buf1, MAXLINE)) > 0) && (buf1[0] != '\r')) {
+    //   if (buf1[0] != 'C') {
+    //     printf("%s", buf1);
+    //     send(serverfd, buf1, MAXLINE, 0);
+    //     send(serverfd, "\r\n", MAXLINE, 0);
+    //   }
+    // }
+    // send(serverfd, "\r\n\r\n", MAXLINE, 0);
+    // // printf("Finish sending client request\n");
 }
 
 
